0dd233b99b79e8ec44e5d9d58a582da5f0494d2f
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2023-09-22 14:03:59 +0200

    Apply standard indentation rules

--- gen/wrapper.jl
+++ gen/wrapper.jl
@@ -6,25 +6,25 @@ using JuliaFormatter
 
 function main()
 
-  cd(@__DIR__)
-  include = joinpath(PRIMA_jll.artifact_dir, "include", "prima")
-  headers = [joinpath(include, "prima.h")]
+    cd(@__DIR__)
+    include = joinpath(PRIMA_jll.artifact_dir, "include", "prima")
+    headers = [joinpath(include, "prima.h")]
 
-  options = load_options(joinpath(@__DIR__, "prima.toml"))
-  options["general"]["output_file_path"] = joinpath("..", "src", "wrappers.jl")
+    options = load_options(joinpath(@__DIR__, "prima.toml"))
+    options["general"]["output_file_path"] = joinpath("..", "src", "wrappers.jl")
 
-  args = get_default_args()
-  push!(args, "-I$include")
-  
-  ctx = create_context(headers, args, options)
-  build!(ctx)
+    args = get_default_args()
+    push!(args, "-I$include")
 
-  path = options["general"]["output_file_path"]
-  format_file(path, YASStyle())
-  return nothing
+    ctx = create_context(headers, args, options)
+    build!(ctx)
+
+    path = options["general"]["output_file_path"]
+    format_file(path, YASStyle())
+    return nothing
 end
 
 # If we want to use the file as a script with `julia wrapper.jl`
 if abspath(PROGRAM_FILE) == @__FILE__
-  main()
+    main()
 end
